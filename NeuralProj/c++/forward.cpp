5fbac7f306543a0f78d5fbaa04ba5342f7bd725a
skylake <sunskybai@gmail.com>
2018-10-31 19:48:53 +0800

    tools中新增加了几个功能函数，对代码进行了部分改写

--- NeuralProj/c++/forward.cpp
+++ NeuralProj/c++/forward.cpp
@@ -4,7 +4,14 @@
 # include "activef.h"
 # include "stdlib.h"
 # include "unistd.h" //sleep(int second)函数库
+
+# define INPUT_SIZE 2    // 输入节点数
+# define HIDE_SIZE 3     // 隐藏层节点数
+# define OUTPUT_SIZE 1   // 输出层节点数
+
+# define LEARNING_RATE 0.01 // 学习率
 using namespace std;
+
 
 int main(){
 	
@@ -13,22 +20,51 @@ int main(){
 	// 产生数据N对数据
 	cte_data();
 	
-	float data[N][2]; // 保存数据信息 x y 
-	// 初始化两个权值
-	float w1 = 0.2, w2 = 0.5, b = 0.2;
-	// 设定一个阈值c
-	float c = 0.7; 
-	get_data(data);
+	float data[N][2]; // 保存从文件中读取的数据信息 x y 
+	// 初始化两组权值和一个偏置（这里最好写一个数组保存）
+	float W1[2][3];	
+	init_W(*W1, 2, 3);
+	cout << "初始W1矩阵为：\n";
+	print (&W1[0][0], 2, 3);
+		
+	float W2[3][2];
+	init_W(*W2, 3, 1);
+	cout << "初始W2矩阵为：\n";
+	print (&W2[0][0], 3, 2);	
 	
+	// 将数据从文本中读取到二维数组data中
+	get_data(data);
+
+	float y_[N][2];
+	// 正确输出的集合y_为二维数组
+	for (int i = 0; i < N; i++) {
+		
+		int x = data[i][0], y = data[i][1];
+		if ((x <= 5 && y <= 5) || (x >= 5 && y >= 5) ) {
+			
+			y_[i][0] = 0;
+			y_[i][1] = 1;
+
+		}else{
+			y_[i][0] = 1;
+			y_[i][1] = 0;
+		}
+	}
+	cout << "正确答案：\n" ;
+	print (&y_[0][0], N, 2);		
+
+	
+	// matmul(data[]);
+	/**
+	float *arr_smax;
 	// 临时数组
-	int val = 0; // 计数用的
 	float arrtemp[N][2];		
 	for (int k = 0; k < N; k++){
   		
-		// 经过累加求和后的输出y
-		// double y = tanh(data[k][0] * w1 + data[k][1] * w2 + b);
-		double y = data[k][0] * w1 + data[k][1] * w2 + b;
+		arr_smax = softmax(data[k], N);
+		cout << "概率分布" << " " << arr_smax[0] << " " << arr_smax[1] << " " << endl;
 		
 	}
+	**/
 	return 0;
 }
--- NeuralProj/c++/tools.h
+++ NeuralProj/c++/tools.h
@@ -2,8 +2,81 @@
 # include "time.h"
 # include "stdio.h"
 # include "stdlib.h"
+# include "math.h"
+# include "unistd.h"
 using namespace std;
-int max(int x, int y){
+// 函数声明
+float _01_rand();
+void print (float *arr, int row, int column);
+void init_W (float *W, int row, int column);
+float *matmul (float *InPut, float *W, int row, int column);
+
+
+
+// 打印数组
+void print (float *arr, int row, int column) {
+
+	 for (int i = 0; i < row; i++) {
+
+                for (int j = 0; j < column; j++) {
+
+                        cout << *(arr + column * i + j) << " ";
+                }
+		cout << endl;
+        } 
+
+}
+// 初始化权重,数组是顺序存储的，二维数组也是
+void init_W (float *W, int row, int column) {
+
+	for (int i = 0; i < row; i++) {
+
+		for (int j = 0; j < column; j++) {
+
+			*(W + column * i + j) = _01_rand();
+		}
+	} 
+}
+
+// 简单模拟矩阵乘法，这里是用两个二维数组相乘，返回一个数值
+/**
+	InPut [0.2 0.3] 1 x 2
+
+	W1 [ 
+     		0.1 0.3 0.5
+     		0.2 0.5 0.6
+   	]  2 x 3
+
+	result = 1 x 3
+
+	[x1 x2 x3]
+
+	W2 [
+     		0.6 0.2
+     		0.1 0.1
+     		0.9 0.4
+   	] 3 x 2
+
+	y = 1 x 2
+**/
+float *matmul (float *InPut, float *W, int row, int column) {
+	
+	float *arr = (float *) malloc(sizeof(float) * column);
+	float sum = 0.0;
+	for (int i = 0; i < column; i++) {
+		
+		for (int j = 0; j < row; j++) {
+		
+			sum += *(W + column * j) * InPut[j];
+
+		}
+		arr[i] = sum;
+	}
+	return arr;
+}
+
+
+int max (int x, int y){
 
 	return x > y ? x : y;
 }
@@ -13,17 +86,27 @@ float _01_rand() {
 	// N可以确定产生的精度，比如需要两位小数，N = 99，三位小数 N = 999
 	int N = 99;
 	// 初始化rand()的起始值(程序调用速度过快的时候，产生的随机数是一样的)
-	srand((unsigned) time(0));
-
+	// srand((unsigned) time(NULL));
 	return rand() % (N + 1) / (float) (N + 1);
 }
 
-void softmax(float *arr_y) {
-
+float* softmax (float *arr_y, int N = 100) {
 	
+	static float arr_temp[100];
+	float sum = 0.0;
+	
+	for (int i = 0; i < N; i++) {
+		
+		sum += exp(arr_y[i]);	
 
+	}
+	for (int j = 0; j < N; j++) {
+		
+		arr_temp[j] = arr_y[j] / sum; 
 
-
+	}
+	
+	return arr_temp;
 }
 // 交叉熵
 void cross_entropy() {
